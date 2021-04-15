4a7ec09cde6aeb75e36d21978046a7c95cd6115a
robin.singla <robinformal@gmail.com>
2021-04-15 21:50:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/searchview/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.searchview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import com.example.searchview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val user = arrayOf("Abhay","Joseph","Maria","Avni","Apoorva","Chris","David","Kaira","Dwayne","Christopher",
+            "Jim","Russel","Donald","Brack","Vladimir")
+
+        val userAdapter : ArrayAdapter<String> = ArrayAdapter(
+            this,android.R.layout.simple_list_item_1,
+            user
+        )
+
+        binding.userList.adapter = userAdapter;
+
+        binding.searchView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                if (user.contains(query)){
+
+                    userAdapter.filter.filter(query)
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                userAdapter.filter.filter(newText)
+                return false
+            }
+
+
+        })
+
+    }
+}
